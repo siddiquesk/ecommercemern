9e16b5715556ebdeb02185bf11edd060bf79e3e7
siddiquesk <mehnajk07@gmail.com>
2025-09-03 20:43:51 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import { ToastContainer, toast } from 'react-toastify';
 function App() {
   return (
     <>
-      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <div className='px-4  sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
         <ToastContainer />
         <Navbar />
         <SearchBar />
